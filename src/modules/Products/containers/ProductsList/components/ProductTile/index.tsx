9bf3255402f1ebe2d81226f205e2bfcc84d24832
Kajetan Dobczyk <kajetan.dobczyk@gmail.com>
2021-06-07 17:21:11 +0200

    add product tile

--- src/modules/Products/containers/ProductsList/components/ProductTile/index.tsx
+++ src/modules/Products/containers/ProductsList/components/ProductTile/index.tsx
@@ -7,14 +7,16 @@ type Props = {
 }
 
 const ProductTile: React.FC<Props> = ({ product }) => (
-  <Link href={`products/${product.id}`}>
-    <a>
-      <h2>{product.title}</h2>
-      <span>${product.price}</span>
-      <p>{product.description}</p>
-      <img src={product.image} alt={product.title} />
-    </a>
-  </Link>
+  <li>
+    <Link href={`products/${product.id}`}>
+      <a>
+        <h2>{product.title}</h2>
+        <span>${product.price}</span>
+        <p>{product.description}</p>
+        <img src={product.image} alt={product.title} />
+      </a>
+    </Link>
+  </li>
 )
 
 export default ProductTile
