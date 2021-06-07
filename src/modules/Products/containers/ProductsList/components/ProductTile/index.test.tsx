604cb965f08eead33ff32b6dfcd9dd447d17dc32
Kajetan Dobczyk <kajetan.dobczyk@gmail.com>
2021-06-07 16:23:53 +0200

    add product tile with test

--- src/modules/Products/containers/ProductsList/components/ProductTile/index.test.tsx
+++ src/modules/Products/containers/ProductsList/components/ProductTile/index.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react'
+import { sampleProduct } from 'src/modules/Product/mocks'
 
 import ProductTile from '.'
 
 test('Product tile renders correctly', async () => {
-  render(<ProductTile />)
+  render(<ProductTile product={sampleProduct} />)
 
-  expect(screen.getByText('Product tile')).toBeInTheDocument()
+  expect(
+    screen.getByRole('heading', { name: /Fjallraven/ }),
+  ).toBeInTheDocument()
+  expect(screen.getByText('$109.95')).toBeInTheDocument()
+  expect(screen.getByRole('img', { name: /Fjallraven/ })).toBeInTheDocument()
+  expect(screen.getByRole('link')).toHaveAttribute('href', '/products/1')
 })
--- src/modules/Products/containers/ProductsList/components/ProductTile/index.tsx
+++ src/modules/Products/containers/ProductsList/components/ProductTile/index.tsx
@@ -1,5 +1,20 @@
-const ProductTile = () => {
-  return <div>Product tile</div>
+import Link from 'next/link'
+
+import { IProduct } from 'src/modules/Product/interfaces'
+
+type Props = {
+  product: IProduct
 }
+
+const ProductTile: React.FC<Props> = ({ product }) => (
+  <Link href={`products/${product.id}`}>
+    <a>
+      <h2>{product.title}</h2>
+      <span>${product.price}</span>
+      <p>{product.description}</p>
+      <img src={product.image} alt={product.title} />
+    </a>
+  </Link>
+)
 
 export default ProductTile
