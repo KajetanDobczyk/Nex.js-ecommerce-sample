b2f831e3eacff148bfe8e7e7191f05c09c4e7106
Kajetan Dobczyk <kajetan.dobczyk@gmail.com>
2021-06-08 13:33:01 +0200

    add product display test

---
+++ src/modules/Product/containers/ProductDetails/index.test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/dom'
+
+import store from 'src/config/redux/store'
+import { renderWithProviders } from 'src/tests/test-utils'
+
+import ProductDetails from '.'
+import { setProduct } from '../../store/slice'
+import { mockedProduct } from '../../mocks'
+
+describe('ProductDetails', () => {
+  test('Displays all required product details on page', () => {
+    renderWithProviders(<ProductDetails />)
+    store.dispatch(setProduct(mockedProduct))
+
+    expect(
+      screen.getByRole('heading', { name: mockedProduct.title }),
+    ).toBeInTheDocument()
+    expect(screen.getByText(mockedProduct.category)).toBeInTheDocument()
+    expect(screen.getByText(mockedProduct.description)).toBeInTheDocument()
+    expect(screen.getByText(`$${mockedProduct.price}`)).toBeInTheDocument()
+    expect(
+      screen.getByRole('img', { name: mockedProduct.title }),
+    ).toBeInTheDocument()
+    expect(screen.getByRole('link')).toHaveAttribute(
+      'href',
+      `/products/${mockedProduct.id}/`,
+    )
+  })
+})
--- src/modules/Products/containers/ProductsList/components/ProductTile/index.test.tsx
+++ src/modules/Products/containers/ProductsList/components/ProductTile/index.test.tsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+
 import { mockedProduct } from 'src/modules/Product/mocks'
 
 import ProductTile from '.'
@@ -7,9 +8,14 @@ test('Product tile renders correctly', async () => {
   render(<ProductTile product={mockedProduct} isOnGrid={true} />)
 
   expect(
-    screen.getByRole('heading', { name: /Fjallraven/ }),
+    screen.getByRole('heading', { name: mockedProduct.title }),
   ).toBeInTheDocument()
-  expect(screen.getByText('$109.95')).toBeInTheDocument()
-  expect(screen.getByRole('img', { name: /Fjallraven/ })).toBeInTheDocument()
-  expect(screen.getByRole('link')).toHaveAttribute('href', '/products/1')
+  expect(screen.getByText(`$${mockedProduct.price}`)).toBeInTheDocument()
+  expect(
+    screen.getByRole('img', { name: mockedProduct.title }),
+  ).toBeInTheDocument()
+  expect(screen.getByRole('link')).toHaveAttribute(
+    'href',
+    `/products/${mockedProduct.id}`,
+  )
 })
